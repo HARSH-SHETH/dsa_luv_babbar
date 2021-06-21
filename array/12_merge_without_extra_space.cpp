71642b511c6e928a8b3afa86afcd176e7c915dc5
harsh <ycdtharsh@gmail.com>
2021-06-21 12:50:41 +0530

    12 merge two sorted arrays without extra space

---
+++ array/12_merge_without_extra_space.cpp
@@ -0,0 +1,18 @@
+void swap(int *a, int *b){
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+void merge(int arr1[], int arr2[], int n, int m) {
+  // code here
+  int j = 0;
+  for(int i = n-1; i >= 0 && j < m;){
+    if(arr1[i] > arr2[j]) {
+      swap(&arr1[i], &arr2[j]);
+      i--;
+    }
+    j++;
+  }
+  sort(arr1, arr1+n);
+  sort(arr2, arr2+m);
+}
